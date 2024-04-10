4101cb8f2e3571c81703c7f049e28a2030b10625
mario.arista <mario.arista.dev@gmail.com>
2024-04-10 14:58:14 +0200

    Adding PageController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
 @extends('layouts/app')
+
+@section('title')
+    Laravel Migration-Seeder
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>
+            ciao a tutti
+        </h1>
+    </div>
+
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>@yield('title')</title>
 
     <!-- Includiamo gli assets con la direttiva vite -->
     @vite('resources/js/app.js')
@@ -12,6 +12,6 @@
 </head>
 
 <body>
-    
+    @yield('content')
 </body>
 </html>
